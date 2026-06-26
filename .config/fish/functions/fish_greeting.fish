62c623523a8738ae4b32094f52afb4270df0f0b3
Ka Thas <ka@kathas.no>
2026-06-26 09:59:06 +0200

    fih

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -6,6 +6,16 @@ function fish_greeting
         "Ready when you are" \
         "Lock in" \
         "Ligma" \
+        "Howdy" \
+        "Full Cowboy" \
+        "Inner peace... dinner please..." \
+        "Quit, don't quit, noodles" \
+        "안녕하세요" \
+        "화이팅!" \
+        "( ´ ∀ `)ノ～ ♡" \
         "Necessity is the mother of invention"
     echo -n (random choice $greetings)
 end
+
+# https://www.emoticonstext.com/
+# https://emojidb.org/cute-japanese-emojis
